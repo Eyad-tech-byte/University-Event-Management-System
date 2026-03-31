9fb7c20bf73765059af64078c3b6db7595546f8f
PixelLogic-wq <bolbenjamin550@gmail.com>
2026-03-31 21:06:14 +0400

    fixing login page

--- P2898534/app/controller/login.js
+++ P2898534/app/controller/login.js
@@ -1,5 +1,5 @@
 import render from "../render.js";
 import { loginView } from "../views/login.js";
 export function loginController ({request}) {
-    return render(loginView, {}, request, 200);
+    return render(loginView(), {}, request, 200);
 }
--- P2898534/app/controller/register.js
+++ P2898534/app/controller/register.js
@@ -2,5 +2,5 @@ import render from "../render.js";
 import { registerview } from "../views/register.js";
 
 export function registerController ({request}) {
-    return render(registerview, {}, request, 200);
+    return render(registerview(), {}, request, 200);
 }
